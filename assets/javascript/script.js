35640ce420c8baa469e09dffa745a7dec022ada1
jhonorlan <jhonorlan22@gmail.com>
2021-03-03 15:23:15 +0800

    update-nanaman

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,18 +8,6 @@ function animate() {
 	const PERCENT = (SCROLLTOP / SCROLLHEIGHT) * 100;
 	const per = parseInt(BALLOON.style.left);
 	BALLOON.style.top = `${PERCENT / 2.05}%`;
-
-	var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-	if (st > lastScrollTop) {
-		// downscroll code
-
-		console.log("down");
-	} else {
-		// upscroll code
-		console.log("up");
-	}
-
-	lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }
 
 addEventListener("scroll", animate);
